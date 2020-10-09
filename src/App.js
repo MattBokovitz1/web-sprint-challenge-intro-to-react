54ba6456915929613790908ba23ead96120b2a5a
Matthew Bokovitz <MattBokovitz@Matthews-MBP-2.fios-router.home>
2020-10-09 13:54:38 -0400

    finished mvp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +10,23 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const[data, setData] = useState([])
+  useEffect(()=>{
+    axios.get(`http//:swapi.dev/api/people/`)
+    .then(res=>{
+      setData(res.data.results)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {data.map(character=>{
+        {return <Character key={character.id} character={character}/>}
+      })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,22 @@
+import { findByLabelText } from '@testing-library/react';
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components';
+const Characterinfo = styled.div;
+    
+
+export default function Character(props){
+    const { character } = props;
+
+return(
+    <Characterinfo>
+        <h2>Name:  {character.name}</h2>
+        <h3>Height:  {character.height}</h3>
+        <h3>Mass:  {character.mass}</h3>
+        <h3>Eye Color:  {character.eye_color}</h3>
+        <h3>Hair Color:  {character.hair_color}</h3>
+        <h3>Birth Year:  {character.birth_year}</h3>
+    </Characterinfo>    
+)
+}    
+        
